caf837517e37063b0ddae74942217e08e1564cc8
zulubit <zaanfr@gmail.com>
2025-07-26 19:46:43 +0200

    remap accept

--- init.lua
+++ init.lua
@@ -763,7 +763,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-Return>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
